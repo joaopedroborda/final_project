047645ac504184d6d9b37af35616ceb176d379df
joaopedroborda <joaobordasilva@isluny.org>
2025-11-28 16:18:44 -0300

    just port incremented

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { CustomError } from './core/errors/custom/base-custom-error'
 
+import { app } from './main/app'
+
 async function main() {
-	
+    app.listen( { port: 8000 }  )
 }
 
 main()
